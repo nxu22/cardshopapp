533e64075f3197d2feaa5c9d2fd64f755287ec0d
nxu22 <nxu2@academic.rrc.ca>
2024-07-11 10:52:34 -0500

    added searching function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,3 @@
-# app/controllers/products_controller.rb
 class ProductsController < ApplicationController
   def index
     if params[:category]
@@ -9,8 +8,15 @@ class ProductsController < ApplicationController
         @products = Product.none
         flash[:alert] = "Category not found"
       end
+    elsif params[:query].present?
+      query = "%#{params[:query].downcase}%"
+      @products = Product.where('lower(name) LIKE ?', query)
     else
       @products = Product.all
     end
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
